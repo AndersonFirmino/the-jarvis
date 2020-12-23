cc93149ef827722cde9c3805918899a337dfb416
AndersonFirmino <anderson.araujoprog@gmail.com>
2020-12-23 00:03:40 -0300

    Ajustes no controller :snake:
    Signed-off-by: AndersonFirmino <anderson.araujoprog@gmail.com>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,12 @@
-import { TextField } from '@material-ui/core'
 import React from 'react'
 import { Controller } from 'react-hook-form'
 
 import InputDto from './Input.dto'
 
+import { TextField } from './styles'
+
 const Input: React.FC<InputDto> = ({ name, control, defaultValue }) => {
-  return <Controller as={TextField} name={name} control={control} defaultValue={defaultValue} />
+  return <Controller name={name} control={control} defaultValue={defaultValue} render={() => <TextField />} />
 }
 
 export default Input
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,7 @@
+import { TextField as MTextField, withStyles } from '@material-ui/core'
+
+export const TextField = withStyles({
+  root: {
+    color: 'red',
+  },
+})(MTextField)
